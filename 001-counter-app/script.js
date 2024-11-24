99575e1410c1bb1e8a88108c3fe6a623f2b6ebd3
Redita <103308192+riditalove@users.noreply.github.com>
2024-11-24 06:46:17 -0800

    Update script.js
    Decrement of the counter

--- 001-counter-app/script.js
+++ 001-counter-app/script.js
@@ -1,6 +1,7 @@
 let count = 0;
 const counterElement = document.getElementById('counter');
 const incrementButton = document.getElementById('increment');
+const decrementButton = document.getElementById('decrement');
 
 function increaseCount() {
   count = count + 1;
@@ -8,3 +9,10 @@ function increaseCount() {
 }
 
 incrementButton.addEventListener('click', increaseCount);
+
+function decreaseCount() {
+  count = count - 1;
+  counterElement.innerText = count;
+}
+
+decrementButton.addEventListener('click', decreaseCount);

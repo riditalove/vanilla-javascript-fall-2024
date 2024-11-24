be64f6289617d1b49f33c6d3af0686cb0a55cbc8
Redita <103308192+riditalove@users.noreply.github.com>
2024-11-24 08:15:44 -0800

    Update script.js
    Counting modification

--- 001-counter-app/script.js
+++ 001-counter-app/script.js
@@ -4,15 +4,26 @@ const incrementButton = document.getElementById('increment');
 const decrementButton = document.getElementById('decrement');
 
 function increaseCount() {
+  if(count>=10)
+  {
+    alert("The max count can't exceed 10");
+  }
+  else{
   count = count + 1;
   counterElement.innerText = count;
+  }
 }
 
 incrementButton.addEventListener('click', increaseCount);
 
 function decreaseCount() {
+  if(count === 0)
+  {
+    alert("The count can't be negative");
+  }
+  else{
   count = count - 1;
-  counterElement.innerText = count;
+  counterElement.innerText = count;}
 }
 
 decrementButton.addEventListener('click', decreaseCount);
